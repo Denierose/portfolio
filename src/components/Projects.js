9c7a870408a8e1be50b4efd46ebe77292c0edceb
Denierose <dele.monsanto.up@phinmaed.com>
2023-08-17 11:15:43 +0800

    added redirection link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,25 +4,31 @@ const Projects = () => {
   return (
     <div className='gallery'>
       <figure class="hover-img">
+        <a href='https://github.com/Denierose' className='text-light'>
         <img src="/upang_updates.jpg" alt='project-1'/>
         <figcaption>
           <h3 className='fw-bold'>UPang Updates</h3>
           <h6>Mobile App that updates you about UPang</h6>
         </figcaption>
+        </a>
       </figure>
       <figure class="hover-img">
-        <img src="/portal.jpg" alt='project-2'/>
-        <figcaption>
-          <h3 className='fw-bold'>PAID</h3>
-          <h6>School Portal Web App for UPang students and faculty</h6>
-        </figcaption>
+        <a href='https://github.com/Denierose' className='text-light'>
+          <img src="/portal.jpg" alt='project-2'/>
+          <figcaption>
+            <h3 className='fw-bold'>PAID</h3>
+            <h6>School Portal Web App for UPang students and faculty</h6>
+          </figcaption>
+        </a>
       </figure>
       <figure class="hover-img">
-        <img src="/cite.png" alt='project-3'/>
-        <figcaption>
-          <h3 className='fw-bold'>CITE Events</h3>
-          <h6>Web Page about events happens in CITE</h6>
-        </figcaption>
+        <a href='https://github.com/Denierose' className='text-light'>
+          <img src="/cite.png" alt='project-3'/>
+          <figcaption>
+            <h3 className='fw-bold'>CITE Events</h3>
+            <h6>Web Page about events happens in CITE</h6>
+          </figcaption>
+        </a>
       </figure>
     </div>
   )
--- src/pages/sections/Contact.js
+++ src/pages/sections/Contact.js
@@ -9,7 +9,9 @@ const Contact = ()=> {
         <Container className="my-5 d-flex flex-column align-items-center text-center">
             <h1 className="fw-bold display-4">Message<span style={{color: "#F5F5DC"}}>Me</span></h1>
             <p>Feel free to reach out to me on my social media below.</p>
-            <p><FaMagento/> dele.monsanto.up@phinmaed.com</p>
+            <a href="mailto:dele.monsanto.up@phinmaed.com" className="text-decoration-none text-light">
+                <FaMagento/> dele.monsanto.up@phinmaed.com
+            </a>
             <SocialLinks />
         </Container>
     );
